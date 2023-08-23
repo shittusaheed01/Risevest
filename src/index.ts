a5bec07647f3458c07aca9c2929b19cbcea6801f
Saheed Shittu <shittusaheed01@gmail.com>
2023-08-23 23:19:30 +0100

    error in index

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,23 @@ const start = async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
-	if (!process.env.PORT) {
-		throw new Error('PORT must be defined');
+	// if (!process.env.PORT) {
+	// 	throw new Error('PORT must be defined');
+	// }
+	if (!process.env.CLOUDINARY_CLOUD_NAME) {
+		throw new Error('CLOUDINARY_CLOUD_NAME must be defined');
+	}
+	if (!process.env.CLOUDINARY_API_KEY) {
+		throw new Error('CLOUDINARY_API_KEY must be defined');
+	}
+	if (!process.env.CLOUDINARY_SECRET_KEY) {
+		throw new Error('CLOUDINARY_SECRET_KEY must be defined');
+	}
+	if (!process.env.CLOUDINARY_URL) {
+		throw new Error('CLOUDINARY_URL must be defined');
+	}
+	if (!process.env.POSTGRES_URL) {
+		throw new Error('POSTGRES_URL must be defined');
 	}
 
 	try {
