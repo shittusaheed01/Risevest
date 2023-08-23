fc20f8f68841c7e62bf37d9d31f03579eb9412ef
Saheed Shittu <shittusaheed01@gmail.com>
2023-08-23 23:37:32 +0100

    live router update

--- src/routes/live.ts
+++ src/routes/live.ts
@@ -3,6 +3,23 @@ import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
 
 router.get(
+	'/',
+	async (req: Request, res: Response, next: NextFunction) => {
+
+		try {
+			res.status(200).json({
+				status: 'success',
+				data: {
+					message: 'Welcome to my cloud storage API',
+				},
+			});
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	}
+);
+router.get(
 	'/api/live',
 	async (req: Request, res: Response, next: NextFunction) => {
 
